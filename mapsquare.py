0f6ed8d6b1ff2b0860f49b69d066b88447e6f4f9
greatvijay <136738331+greatvijay@users.noreply.github.com>
2023-07-21 23:48:49 +0530

    Add files via upload

---
+++ mapsquare.py
@@ -0,0 +1,5 @@
+sample_list = [4,5,2,9] # Sample list of integer
+squared_numbers = list(map(lambda x: x **2, sample_list))#Using map function
+
+print("Original list:", sample_list)
+print("Squared list:", squared_numbers)
